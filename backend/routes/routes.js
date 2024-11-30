ae48bb418112f017137a9b3c67deb5bcd264e8b1
JedeDChicken <daniellecastor071@gmail.com>
2024-11-30 17:00:30 +0800

    update

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -10,54 +10,54 @@ router.get('/', (req, res) => {
     res.json("Hello");
 })
 
-router.post('/login', async (req, res) => {
-    const { email, pass } = req.body;
+// router.post('/login', async (req, res) => {
+//     const { email, pass } = req.body;
 
-    try {
-        const user = await AuthModel.findOne({ email: email });
+//     try {
+//         const user = await AuthModel.findOne({ email: email });
 
-        if (user) {  // If user exists
-            const isMatch = await bcryptjs.compare(pass, user.pass);  // Compare
-            if (isMatch) {
-                return res.status(200).json('Success!');
-            }
-            else {
-                return res.status(401).json('The password is incorrect');
-            }
-        }
-        else {
-            return res.status(404).json('The email is not registered');
-        }
-    }
-    catch (err) {
-        return res.status(500).json({ message: 'Internal server error', error: err.message });
-    }
-});
+//         if (user) {  // If user exists
+//             const isMatch = await bcryptjs.compare(pass, user.pass);  // Compare
+//             if (isMatch) {
+//                 return res.status(200).json('Success!');
+//             }
+//             else {
+//                 return res.status(401).json('The password is incorrect');
+//             }
+//         }
+//         else {
+//             return res.status(404).json('The email is not registered');
+//         }
+//     }
+//     catch (err) {
+//         return res.status(500).json({ message: 'Internal server error', error: err.message });
+//     }
+// });
 
-router.post('/register', async (req, res) => {
-    const { name, email, pass } = req.body;
+// router.post('/register', async (req, res) => {
+//     const { name, email, pass } = req.body;
 
-    try {
-        const user_name = await AuthModel.findOne({ name: name });
-        const user_email = await AuthModel.findOne({ email: email });
+//     try {
+//         const user_name = await AuthModel.findOne({ name: name });
+//         const user_email = await AuthModel.findOne({ email: email });
 
-        if (user_email) {
-            return res.status(400).json('The email is already taken. Choose a different one.');
-        }
-        if (user_name) {
-            return res.status(400).json('The name is already taken. Choose a different one.');
-        }
+//         if (user_email) {
+//             return res.status(400).json('The email is already taken. Choose a different one.');
+//         }
+//         if (user_name) {
+//             return res.status(400).json('The name is already taken. Choose a different one.');
+//         }
 
-        // If both are unique
-        // if (!user_name && !user_email) {  // If email's not yet taken
-        const hashedPass = await bcryptjs.hash(pass, 10);  // 10- # salt rounds
-        const newUser = { name, email, pass: hashedPass };
-        await AuthModel.create(newUser);
-        return res.status(201).json('Success!');
-    }
-    catch (err) {
-        return res.status(500).json({ message: 'Internal server error', error: err.message });
-    }
-});
+//         // If both are unique
+//         // if (!user_name && !user_email) {  // If email's not yet taken
+//         const hashedPass = await bcryptjs.hash(pass, 10);  // 10- # salt rounds
+//         const newUser = { name, email, pass: hashedPass };
+//         await AuthModel.create(newUser);
+//         return res.status(201).json('Success!');
+//     }
+//     catch (err) {
+//         return res.status(500).json({ message: 'Internal server error', error: err.message });
+//     }
+// });
 
 export default router;
